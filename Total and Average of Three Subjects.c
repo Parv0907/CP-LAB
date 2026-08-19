812e82feede3bb63851a07e66155537c8db01f71
Parv0907 <patelparv49@gmail.com>
2026-08-19 12:25:37 +0530

    Total and Average of Three Subjects

---
+++ Total and Average of Three Subjects.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main( )
+{ /* Total & average marks of 3 subjects */
+int s1, s2, s3, total;
+float avg;
+printf("Enter marks of 3 subjects : ");
+scanf("%d %d %d", &s1, &s2, &s3);
+total = s1 + s2 + s3;
+avg = total / 3.0;
+printf("Total = %d\n", total);
+printf("Average = %.2f\n", avg);
+}
