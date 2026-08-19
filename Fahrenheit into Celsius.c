e2aa0754bd29ec6d41bb6d1cc97fa5afd24f9ede
Parv0907 <patelparv49@gmail.com>
2026-08-19 12:20:13 +0530

    Convert Fahrenheit into Celsius
    C = 5/9 * (F - 32)

---
+++ Fahrenheit into Celsius.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main( )
+{ /* Convert Fahrenheit into Celsius */
+float f, c;
+printf("Enter temperature in Fahrenheit : ");
+scanf("%f", &f);
+c = (5.0 / 9.0) * (f - 32);
+printf("%.2f F = %.2f C\n", f, c);
+}
